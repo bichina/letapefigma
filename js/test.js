0bd9b426bc036121c8216cf3110a40f178ca39ba
bichina <bichina0312@gmail.com>
2022-09-12 19:55:32 +0900

    js file upload

---
+++ js/test.js
@@ -0,0 +1,14 @@
+$('.slick-carousel').slick({
+  centerMode: false,
+  slidesToShow: 2.1,
+  dots: true,
+  autoplay: true,
+  autoplaySpeed: 3000,
+  pauseOnHover: true,
+  pauseOnDotsHover: true,
+  edgeFriction: 0.05,
+  infinite: true, 
+  dotsClass : "slick-dots",
+  
+
+});
