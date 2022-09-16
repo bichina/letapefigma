f884afc1b2fe79beb01e65fe8c710b7faaf17c2b
bichina <bichina0312@gmail.com>
2022-09-16 11:45:42 +0900

    수정사항3

---
+++ js/sub.js
@@ -0,0 +1,11 @@
+/* 오시는 길 */
+$(function () {
+  $("#tab1").click(function () {
+    $("#content3").show().siblings("#content4").hide();
+  });
+});
+$(function () {
+  $("#tab2").click(function () {
+    $("#content4").show().siblings("#content3").hide();
+  });
+});
