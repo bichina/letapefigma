64fb221201e52ead01326939f053554d1579c45c
bichina <bichina0312@gmail.com>
2022-09-17 22:39:18 +0900

    mobile main work

--- m/js/sub.js
+++ m/js/sub.js
@@ -1,13 +1,19 @@
-// 팝업 열기
-$(document).on("click", ".btn-open", function (e) {
-  var target = $(this).attr("href");
-  $(target).addClass("show");
-});
+// // 팝업 열기
+// $(document).on("click", ".btn-open", function (e) {
+//   var target = $(this).attr("href");
+//   $(target).addClass("show");
+// });
 
-// 외부영역 클릭 시 팝업 닫기
-$(document).mouseup(function (e) {
-  var LayerPopup = $(".layer-popup");
-  if (LayerPopup.has(e.target).length === 0) {
-    LayerPopup.removeClass("show");
-  }
-});
+// // 외부영역 클릭 시 팝업 닫기
+// $(document).mouseup(function (e) {
+//   var LayerPopup = $(".layer-popup");
+//   if (LayerPopup.has(e.target).length === 0) {
+//     LayerPopup.removeClass("show");
+//   }
+// });
+
+$( document ).ready( function() {
+  $( '#classBtn1' ).click( function() {
+    $( '#test' ).slideUp();
+  } );
+} );
