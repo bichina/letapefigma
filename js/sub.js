3088371b30f5fbf30c9800f1c84556e0f688abd7
bichina <bichina0312@gmail.com>
2022-09-19 18:25:45 +0900

    모바일 메인 작업

--- js/sub.js
+++ js/sub.js
@@ -1,11 +1,12 @@
-/* 오시는 길 */
-$(function () {
-  $("#tab1").click(function () {
-    $("#content3").show().siblings("#content4").hide();
-  });
+// 오시는 길
+
+$(function() {
+  $('#tab1').click(function() {
+    $('#content3').show().siblings('#content3').hide();
+  })
 });
-$(function () {
-  $("#tab2").click(function () {
-    $("#content4").show().siblings("#content3").hide();
-  });
+$(function() {
+  $('#tab2').click(function() {
+    $('#content4').show().siblings('#content4').hide();
+  })
 });
--- m/js/sub.js
+++ m/js/sub.js
@@ -23,3 +23,16 @@ $( document ).ready( function() {
     $( '.classBoxRight' ).slideUp();
   } );
 } );
+
+//오시는 길
+
+$(function() {
+  $('#tab1').click(function() {
+    $('#content3').show().siblings('#content4').hide();
+  })
+});
+$(function() {
+  $('#tab2').click(function() {
+    $('#content4').show().siblings('#content3').hide();
+  })
+});
