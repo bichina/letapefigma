3ae1108834e4f1f25161723e87bff301b05bc73c
bichina <bichina0312@gmail.com>
2022-09-20 00:24:43 +0900

    오시는 길 쿼리 오류

--- js/sub.js
+++ js/sub.js
@@ -2,11 +2,11 @@
 
 $(function() {
   $('#tab1').click(function() {
-    $('#content3').show().siblings('#content3').hide();
+    $('#content3').show().siblings('#content4').hide();
   })
 });
 $(function() {
   $('#tab2').click(function() {
-    $('#content4').show().siblings('#content4').hide();
+    $('#content4').show().siblings('#content3').hide();
   })
 });
