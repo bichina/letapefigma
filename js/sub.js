619626e8ad2efeef5da61a5030578d09fe95081d
bnkim <bichina0312@gmail.com>
2022-12-01 17:30:47 +0900

    슬라이더 테스트

--- js/sub.js
+++ js/sub.js
@@ -1,12 +1,30 @@
 // 오시는 길
 
-$(function() {
-  $('#tab1').click(function() {
-    $('#content3').show().siblings('#content4').hide();
-  })
+$(function () {
+  $("#tab1").click(function () {
+    $("#content3").show().siblings("#content4").hide();
+  });
 });
-$(function() {
-  $('#tab2').click(function() {
-    $('#content4').show().siblings('#content3').hide();
-  })
+$(function () {
+  $("#tab2").click(function () {
+    $("#content4").show().siblings("#content3").hide();
+  });
+});
+
+var menu = ["Slide 1", "Slide 2", "Slide 3"];
+var mySwiper = new Swiper(".swiper-container", {
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+    renderBullet: function (index, className) {
+      return '<span class="' + className + '">' + menu[index] + "</span>";
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
 });
